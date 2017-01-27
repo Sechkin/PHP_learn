dd55ded864c0243ac67e7357a3081a7edbf20606
Vladimir <sechkin.vlad@gmail.com>
2017-01-27 14:52:02 +0200

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>php</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            echo "<h1>Hello World!</h1>";
+			echo 'Hello World!';
+			echo "<br>";
+			$name = 'Вова';
+			$age = 33;
+			$data = 'Меня зовут '.$name.', мне '.$age.'годика';
+			echo $data;
+			echo "<br>";
+			$dataWithout = "Меня зовут $name, мне $age годика";
+			echo $dataWithout;
+        ?>
+    </body>
+</html>
